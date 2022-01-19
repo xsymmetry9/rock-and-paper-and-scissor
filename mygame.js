7ff8fd46f7b5136e81b0fbd31b79be226820e543
xsymmetry <glswim@gmail.com>
2022-01-20 01:00:24 +0800

    index.html

---
+++ mygame.js
@@ -0,0 +1,14 @@
+// rock = 1, paper = 2, scissor = 3
+console.log("hello world!")
+/*function computerPlay(){
+    // play[0] = rock
+    // play[1] = paper
+    //play [2] = scissor
+
+const play = ["rock", "paper", "scisoor"];
+    
+    // Randomnize from 0 - 2
+    return play[Math.random()];
+}
+console.log(computerPlay())
+*/
