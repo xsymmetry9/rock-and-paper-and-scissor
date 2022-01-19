d6142a54ed88e8ea806298edfbf4ca329fb2c7b5
xsymmetry <glswim@gmail.com>
2022-01-20 02:23:34 +0800

    mygame.js

--- mygame.js
+++ mygame.js
@@ -1,14 +1,62 @@
 // rock = 1, paper = 2, scissor = 3
-console.log("hello world!")
-/*function computerPlay(){
+function computerPlay(){
     // play[0] = rock
     // play[1] = paper
     //play [2] = scissor
 
-const play = ["rock", "paper", "scisoor"];
-    
+const play = ["rock", "paper", "scissor"];
+const n = Math.floor(Math.random()*2);
     // Randomnize from 0 - 2
-    return play[Math.random()];
+    return play[n];
 }
-console.log(computerPlay())
-*/
+
+function playRound(playerSelection, computerSelection){
+    const player = playerSelection.toLowerCase();
+    const computer = computerSelection.toLowerCase();
+
+    /* rock beats paper
+    paper beats scissor
+    scissor beats paper
+    */
+
+   if (player === "rock" && computer === "paper"){
+       return true;}
+       else if (player === "paper" && computer === "scissor"){
+           return true;}
+           else if (player ==="scissor" && computer === "rock"){
+               return true;}
+           else {
+               return false;}
+}
+
+function game(){
+    const pPlayer = 0;
+    const pComputer = 0;
+    const i = 0;
+    while(i < 5){
+        const playerSelection = prompt();
+        const computerSelection = computerPlay();
+        i++;
+    
+        if (playRound(playerSelection, computerSelection)=== true){
+            pPlayer++
+            return "Player wins";
+        }
+        else{
+            pComputer++;
+            return "Computer wins";
+        }
+    
+    }
+
+    if (pPlayer > pComputer)
+    {
+        return ("Player wins");
+    }
+    else{
+        return("Computer wins");
+    }
+}
+
+
+console.log(game());
