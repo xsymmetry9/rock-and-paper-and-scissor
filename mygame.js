3e8bd808130426e31721951fecebc91f4210a53d
xsymmetry <glswim@gmail.com>
2022-02-25 00:03:41 +0800

    index.html

--- mygame.js
+++ mygame.js
@@ -1,51 +1,82 @@
-// rock = 1, paper = 2, scissor = 3
 function computerPlay(){
-    // play[0] = rock
-    // play[1] = paper
-    //play [2] = scissor
-
-const play = ["rock", "paper", "scissor"];
-const n = Math.floor(Math.random()*2);
-    // Randomnize from 0 - 2
+    const play = ["rock","paper","scissor"];
+    const n = Math.floor(Math.random()*3);
+    // Randomnize from 0-2
     return play[n];
 }
-
+//Checks the winner - returns either human or computer
 function playRound(playerSelection, computerSelection){
-    const player = playerSelection.toLowerCase();
-    const computer = computerSelection.toLowerCase();
-
-    /* rock beats paper
-    paper beats scissor
-    scissor beats paper
-    */
-   if (player === "rock" && computer === "paper"){
-       return ("You win! Rock beats paper");}
-       else if (player === "paper" && computer === "scissor"){
-           return ("You win! Paper beats scissor");}
-           else if (player ==="scissor" && computer === "rock"){
-               return("You win! Scissor beat rock");
-           }
-           else {
-               return("You lose!")
-           }
-}
-
-function game(){
-    const playerSelection =prompt();
+   
+        if ((playerSelection == "rock" && computerSelection == "scissor") ||
+        (playerSelection == "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissor" & computerSelection == "paper")){
+            return "human";
+        }
+        else if(playerSelection == computerSelection){
+         
+            return "computer";
+        }
+        else {
+            return "no winners";
+        }
+    }
+    //convert args to string
+function convert(playerSelection){
     const computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection));
-
+    if (playerSelection == "rock")
+    {
+        return (playRound("rock", computerSelection));
+    }
+    else if (playerSelection == "paper")
+    {
+        return (playRound("paper", computerSelection));
+    }
+    else if (playerSelection == "scissor"){
+        return (playRound("scissor", computerSelection));
+    }
 }
+let humanScore = 0;
+let computerScore = 0;
+const human = document.querySelector('#player1');
+const comp = document.querySelector('#player2');
+const winner = document.querySelector('.winner');
+function checkWinner(humanScore, computerScore){
+    if (humanScore > computerScore)
+    {
+        return "human";
+    }
+    else{return "computer";}
+}
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) =>{
+    button.addEventListener('click', function (e){
+        
+        const theWinner = convert(button.id);
+        if (theWinner == "human"){
+            humanScore += 1;
+            human.textContent = humanScore;
+        }
+        else if(theWinner == "computer"){
+            computerScore += 1;
+            comp.textContent = computerScore;
+        }
+        //reset score
+        const sum = humanScore + computerScore;
+       //console.log(sum);
+       if(sum == 5)
+       {
+           winner.textContent = (checkWinner(humanScore, computerScore) + 
+           " is the winner");
+           humanScore = 0;
+           computerScore = 0;
+           comp.textContent = computerScore;
+           human.textContent = humanScore;
+       }
 
-game();
-game();
-game();
-game();
-game();
-
-
-
-
+   
+        
+    });
+});
 
 
 
