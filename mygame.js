1260c18c48a6b6d60b6ea44d82dca16a03feec1e
Gary <glswim@gmail.com>
2022-07-19 02:03:53 +0800

    1. Changed the order of the layout.     - Scoreboard is at the top with Title-box and is hidden. 2. Added a display property hidden for scoreboard, replay on CSS 3. Added a start button which changes the title-box display property to none and changes scoreBoard diplay to flex. 4. Added more button properties - when hovering the button, it expands a scale of 1.1 larger and changes the opacity to .1 less. 5. Added a replay button. 6. Added another variable called round

--- mygame.js
+++ mygame.js
@@ -3,20 +3,25 @@ const human = document.querySelector('#player1');
 const comp = document.querySelector('#player2');
 const winner = document.querySelector('.winner');
 const message = document.querySelector('.message');
+const roundNumber = document.querySelector('#roundNumber');
+const replayButton = document.querySelector('#reset');
+const startButton = document.querySelector('#start');
 let round = 0;
 let humanScore = 0;
 let computerScore = 0;
+let isThereWinner = false;
 
-
+//Generate a random number from 1 - 3
 function randomNumber(){
     const num = Math.floor(1 + Math.random()*3);
-    return num; //returns a random string
+    return num; 
 }
 
-//Keeps track of the winner
+//Finds the winner of the round
 function getWinner(playerSelection, computerSelection){
     const player = playerSelection;
     const computer = computerSelection;
+    round+=1; //Adds one every time this code runs
 
     if ((player == 1 && computer == 2) || (player == 2 && computer == 1) || (player == 3 && computer == 2))
     {
@@ -27,8 +32,7 @@ function getWinner(playerSelection, computerSelection){
         return "computer";
     }
 }
-    //We get args when it comes from a button
-    //Creates a function that takes an arg and converts it
+    //Creates a function that takes an arg and converts it to int
 function translator(playerSelection){
     let num;
     switch(playerSelection){
@@ -74,7 +78,7 @@ function getMessage(isWinner){
      }
 }
 
-function updateScore(isWinner){
+function getScore(isWinner){
     switch(isWinner){
         case "player":
             humanScore += 1;
@@ -90,80 +94,93 @@ function updateScore(isWinner){
 }
 //checks the winner
 function checkWinner(){
-    if (humanScore == 5){ return "human";}
-    else if(computerScore == 5) {return "computer";}
+    if (humanScore == 5 || computerScore == 5){ 
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+function update()
+{
+    human.textContent = humanScore; //updates Human score
+    comp.textContent = computerScore; // Updates Computer score
+    roundNumber.textContent = round;
+
 }
 
-const buttons = document.querySelectorAll('button');
+
+const buttons = document.querySelectorAll('.buttons');
 buttons.forEach((button) =>{
     button.addEventListener('click', function (e){
-
+//If player has 5 points, game ends
+//Gives the player to choose an option
         if(humanScore == 5 || computerScore == 5){
-            alert("Please reload");
+            alert("Please press replay to play again");
+       
         }
         else{
+            message.style.display ="block";
 
             const player = translator(button.value); //returns an int (1-3)
 
             let game = getWinner(player, randomNumber()); //returns the winner
-            updateScore(game);
-            round+=1;
-            console.log(round);
-            human.textContent = (humanScore); //updates Human score
-            comp.textContent = (computerScore); // Updates Computer score
+            getScore(game); //adds a point to a winner
+            update(); //updates player, computer, and round
             message.textContent = getMessage(game); //Post message on HTML
 
-            
-            if(checkWinner() == "human"){
-                winner.textContent = "Human dominate the world!"
-            }
-            else if(checkWinner() == "computer"){
-                winner.textContent = "Computers dominate the world. Rescue the humans!!!"
-            }
+            if(checkWinner() != false){ //If there is a winner, check who is the winner
+
+                if(humanScore == 5){
+                    message.textContent = "Human dominate the world!"
+                    winner.style.display ="none";
+                    replayButton.style.display ="block";
+    
+                }
+                else{
+                    message.textContent = "Computers dominate the world. Rescue the humans!!!"
+                    winner.style.display="none";
+                    replayButton.style.display ="block";
+                }
+
+            } //Else, hit it one more time
+           
             else{
                 console.log(getMessage(game));
              console.log("Hit it!")   
             }
-    
         }
 
-   
-
-        
-    
-        
-     
-
-
-
-    //     if (theWinner == "human"){
-    //         humanScore += 1;
-    //         human.textContent = humanScore;
-    //     }
-    //     else if(theWinner == "computer"){
-    //         computerScore += 1;
-    //         comp.textContent = computerScore;
-    //     }
-    //     //reset score
-    //     const sum = humanScore + computerScore;
-    //    //console.log(sum);
-    //    if((humanScore == 5) || (computerScore == 5))
-    //    {
-    //        winner.textContent = (checkWinner(humanScore, computerScore) + 
-    //        " is the winner");
-    //        humanScore = 0;
-    //        computerScore = 0;
-    //        comp.textContent = computerScore;
-    //        human.textContent = humanScore;
-    //    }
-
-    // console.log(game); 
-    // console.log("Player Score is:" + playerScore);
-    // console.log("Computer Score is: " + computerScore);
-
-        
     });
 });
 
+replayButton.addEventListener('click', replay);
+
+function replay(){
+    round = 0;
+    humanScore = 0;
+    computerScore = 0;
+    update();
+    message.textContent = "Rematch!"
+    replayButton.style.display ="none";
+    winner.style.display="block";
+
+}
+const scoreBoard = document.querySelector(".scoreBoard");
+const titleBox = document.querySelector('#title');
+
+function start(){
+    titleBox.style.display ="none";
+    scoreBoard.style.display="flex";
+
+    buttons.forEach((button)=> {
+        button.disabled = false;
+    });
+    startButton.style.display ="none";
+    winner.style.display = "block";
+
+}
+//Start Button Section
+startButton.addEventListener('click', start);
 
 
